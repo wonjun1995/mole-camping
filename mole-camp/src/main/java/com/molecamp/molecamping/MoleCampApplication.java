4b2553988852558423e5492180f3f57c5017688b
wonjun1995 <wonjun9506@gmail.com>
2021-11-01 16:53:04 +0900

    spring initializing

---
+++ mole-camp/src/main/java/com/molecamp/molecamping/MoleCampApplication.java
@@ -0,0 +1,13 @@
+package com.molecamp.molecamping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoleCampApplication.class, args);
+    }
+
+}
---
+++ mole-camp/src/test/java/com/molecamp/molecamping/MoleCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molecamp.molecamping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoleCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
